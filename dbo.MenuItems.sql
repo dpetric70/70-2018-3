a592c201ebcfb2b25711d21058e6718b650d957d
dpetric70 <70-2018@ftn.edu.rs>
2020-12-03 15:53:49 +0100

    Kreirana tabela MenuItems

---
+++ dbo.MenuItems.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[MenuItems]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Title] NVARCHAR(200) NOT NULL, 
+    [Description] NVARCHAR(MAX) NULL, 
+    [Price] DECIMAL(18, 2) NOT NULL,
+)
